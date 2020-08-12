635e0c6be9dff336fb69bb7ecd8365e2a124198e
Joshua <joshua1466587594@outlook.com>
2020-08-12 15:10:05 +0800

    feat: add more outputs

--- action.js
+++ action.js
@@ -1,14 +1,27 @@
 const core = require("@actions/core");
 var moment = require('moment');
 
-function action() {
+function action () {
     try {
-        const time = new Date().toISOString();
-        core.setOutput("time", time);
-
-        const format = core.getInput('format', { required: false });
         const utcOffset = core.getInput('utcOffset', { required: false });
-        core.setOutput("formattedTime", moment().utcOffset(utcOffset).format(format));
+        const format = core.getInput('format', { required: false });
+
+        const time = moment().utcOffset(utcOffset);
+
+        core.setOutput("time", time.toISOString());
+        core.setOutput("ISOTime", time.toISOString());
+        core.setOutput("readableTime", time.toString());
+        core.setOutput("formattedTime", time.format(format));
+
+        let [year, month, day, hour, minute, second, millisecond] = time.toArray();
+        month = String(Number(month) + 1);
+        core.setOutput("year", year)
+        core.setOutput("month", month)
+        core.setOutput("day", day)
+        core.setOutput("hour", hour)
+        core.setOutput("minute", minute)
+        core.setOutput("second", second)
+        core.setOutput("millisecond", millisecond)
     } catch (error) {
         core.setFailed(error.message);
     }
